90c220785515fe49e7732943e829da996e502f52
hidehiroaihara <70199100+hidehiroaihara@users.noreply.github.com>
2020-09-06 00:47:28 +0900

    ビューの属性の変更

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
+describe User do
+  before do
+    @user = FactoryBot.build(:user)
+  end
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ''
+
+end
 end
