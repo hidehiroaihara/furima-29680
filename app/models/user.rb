3faef9ded874fdabdbd54511d2d8be82b0fe33b7
hidehiroaihara <70199100+hidehiroaihara@users.noreply.github.com>
2020-09-05 22:56:05 +0900

    カラム名、バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: '全角カタカナのみ入力して下さい' } do
+    validates :family_name_cana
+    validates :first_name_cana
+  end
+  validates :birthday, presence: true
+
+  
+  
 end
