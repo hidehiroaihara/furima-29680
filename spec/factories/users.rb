1f070e5335a9e18c77dea22da934b374528d9245
hidehiroaihara <70199100+hidehiroaihara@users.noreply.github.com>
2020-09-06 20:34:18 +0900

    サインイン機能、factoryBotコードの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,13 +4,10 @@ FactoryBot.define do
     email                  {Faker::Internet.free_email}
     password               {Faker::Internet.password(min_length: 6)}
     password_confirmaiton  {password}
-    
-      transient do
-        person { Gimei.name }
-      end
-      last_name { person.last.kanji }
-      first_name { person.first.kanji }
-      last_name_kana { person.last.hiragana }
-      first_name_kana { person.first.hiragana }
+    family_name            { '田中' }
+    first_name             { '清隆' }
+    family_name_cana       { 'タナカ' }
+    first_name_kana        { 'キヨタカ' }
+    birthday               {Faker::Date.birthday(min_age: 5, max_age: 90)}
   end
 end
