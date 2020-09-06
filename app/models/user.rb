70bb0db0f5c5e86f37f24c51cd82c4822e7b7d97
hidehiroaihara <70199100+hidehiroaihara@users.noreply.github.com>
2020-09-06 22:27:11 +0900

    単体モデルテストの実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  with_options presence: true do
-     validates :nickname
-     validates :birthday
+   with_options presence: true do
+      validates :nickname
+      validates :birthday
 
-     with_options format: { with: /\A[ぁ-んァ-ンー-龥]+\z/, message: '全角文字を使用してください' } do
-       validates :family_name
-       validates :first_name
-     end
+      with_options format: { with: /\A[ぁ-んァ-ンー-龥]+\z/, message: '全角文字かな/カナ/漢字を使用してください' } do
+        validates :family_name
+        validates :first_name
+      end
   
-     with_options format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: '全角カタカナのみ入力して下さい' } do
+     with_options format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/, message: '全角カタカナのみ入力して下さい' } do
        validates :family_name_cana
        validates :first_name_cana
      end
