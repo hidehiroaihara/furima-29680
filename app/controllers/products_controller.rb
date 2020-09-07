489278b58a612a60c7e175fe107607e00f230f13
hidehiroaihara <70199100+hidehiroaihara@users.noreply.github.com>
2020-09-07 17:00:12 +0900

    コントローラーコード記述

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,11 @@ class ProductsController < ApplicationController
     prodcut = Product.find(params[:id])
     product.destory
   def edit
-    
+    @product = Product.find(params[:id])
+  end
+  def update
+    product = Product.find(params[:id])
+    product.update
   private
   def product_params
     params.require(:product).permit(:name, :content, :category_id, :products_condition_id, :shipping_id, :delivery_fee_id, :delivery_day_id, :price, :user_id)
