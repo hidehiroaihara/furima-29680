9c4ff6dbe850e58b7ec441c7ce026cfb79752f60
hidehiroaihara <70199100+hidehiroaihara@users.noreply.github.com>
2020-09-08 18:13:46 +0900

    出品機能実装訂正1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,29 +19,29 @@ class ProductsController < ApplicationController
     end
   end
 
-  def destroy
-    product = Product.find(params[:id])
-    product.destory
-  end
+  # def destroy
+  #   product = Product.find(params[:id])
+  #   product.destory
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-    product = Product.find(params[:id])
-    product.update
-  end
+  # def update
+  #   product = Product.find(params[:id])
+  #   product.update
+  # end
 
-  def show
-  end
+  # def show
+  # end
   
   private
   def product_params
     params.require(:product).permit(:name, :content, :category_id, :products_condition_id, :shipping_id, :delivery_fee_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
   end
-  def set_product
-    @product = Product.find(params[:id])
-  end
+  # def set_product
+  #   @product = Product.find(params[:id])
+  # end
   def move_to_index
       redirect_to action: :index unless user_signed_in?
   end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,46 +9,6 @@ describe Product  do
         it "name、content、category_id、products_condition_id、shipping_id、delivery_fee_id、delivery_day_id、price、imageが存在すれば登録できる" do
           expect(@product).to be_valid
         end
-        it "出品画像が存在する時登録できる" do
-          @product.image = fixture_file_upload('public/images/test_image.png') 
-          expect(@product).to be_valid
-        end
-        it "商品名が存在する時登録できる" do
-           @product.name = "可愛いわんちゃん"
-           expect(@product).to be_valid
-        end
-        it "商品の説明が存在する時登録できる" do
-          @product.content = "いつも外で遊んでます"
-          expect(@product).to be_valid
-        end
-        it "カテゴリーが存在する時登録できる" do
-          @product.category_id = 5
-          expect(@product).to be_valid
-        end
-        it "商品の状態が存在する時登録できる" do
-          @product.products_condition_id = 3
-          expect(@product).to be_valid
-        end
-        it "配送料の負担が存在する時登録できる" do
-          @product.delivery_fee_id = 2
-          expect(@product).to be_valid
-        end
-        it "発送元の地域が存在する時登録できる" do
-          @product.shipping_id = 3
-          expect(@product).to be_valid
-        end
-        it "発送までの日数が存在する時登録できる" do
-          @product.delivery_day_id = 2
-          expect(@product).to be_valid
-        end
-        it "販売価格が300~9999999の間であれば登録できる" do
-          @product.price = "3948595"
-          expect(@product).to be_valid
-        end
-        it "販売価格が半角数字であれば登録できる" do
-          @product.price = 23345
-          expect(@product).to be_valid
-        end
       end
     end  
       context '商品出品がうまくいかないとき' do
@@ -121,6 +81,11 @@ describe Product  do
           @product.price = "お金"
           @product.valid?
           expect(@product.errors.full_messages).to include("Price is not a number")
+        end
+        it "販売価格が300~9999999の間以外だと登録できない" do
+          @product.price = 23
+          @product.valid?
+          expect(@product.errors.full_messages).to include("Price must be greater than or equal to 300")
         end
         it "出品画像空では登録できない" do
           @product.image = nil
