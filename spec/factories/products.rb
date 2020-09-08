30d47a6f22eeace47a9ffd627222419705219b4e
hidehiroaihara <70199100+hidehiroaihara@users.noreply.github.com>
2020-09-08 14:42:53 +0900

    Activestorage

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,11 +2,11 @@ FactoryBot.define do
   factory :product do
     name                       { '可愛いワンチャン' }
     content                    { Faker::Lorem.sentence }
-    category_id            rand{ 1..11 }
-    products_condition_id  rand{ 1..7 }
-    shipping_id            rand{ 1..48 }
-    delivery_fee_id        rand{ 1..4 }
-    delivery_day_id        rand{ 1..4 }
-    price                  rand{ 300..9999999}
+    category_id                {rand (1..11) }
+    products_condition_id      {rand (1..7) }
+    shipping_id                {rand (1..48) }
+    delivery_fee_id            {rand (1..4) }
+    delivery_day_id            {rand (1..4) }
+    price                      {rand (300..9999999) }
   end
 end
